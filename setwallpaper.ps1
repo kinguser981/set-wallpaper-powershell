822ff39bb82c1667bdc771a86d252e29f4e89c62
Samuel Kendall <kinguser981@gmail.com>
2024-10-13 12:44:22 +0330

    Create setwallpaper.ps1

---
+++ setwallpaper.ps1
@@ -0,0 +1,20 @@
+Invoke-WebRequest https://github.com/kinguser981/Personal_files/releases/download/11312652930/my_edit.jpg -OutFile c:\my_edit.jpg
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper
+{
+  public const int SetDesktopWallpaper = 20;
+  public const int UpdateIniFile = 0x01;
+  public const int SendWinIniChange = 0x02;
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+  public static void SetWallpaper(string path)
+  {
+    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+  }
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+
+[Wallpaper]::SetWallpaper("C:\my_edit.jpg")
